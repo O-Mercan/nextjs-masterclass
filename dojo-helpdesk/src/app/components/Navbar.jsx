3f749797770632eb905c4dfbfb9b3efa4c1a161b
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-23 01:10:55 +0300

    added lecture 5:Styles Fonts and Images

--- dojo-helpdesk/src/app/components/Navbar.jsx
+++ dojo-helpdesk/src/app/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import Link from 'next/link'
-
-import React from 'react'
+import Image from 'next/image'
+import Logo from './logo.png'
 
 export default function Navbar() {
   return (
-    <div>
-      <nav>
-          <h1>Dojo Helpdesk</h1>
-          <Link href="/">Dashboard</Link>
-          <Link href="/tickets">Tickets</Link>
-        </nav>
-    </div>
+    <nav>
+      <Image
+        src={Logo}
+        alt='Dojo Helpdesk logo'
+        width={70}
+        placeholder='blur'
+        quality={100}
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
   )
 }
