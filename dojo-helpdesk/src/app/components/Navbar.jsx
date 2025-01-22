07c5d8ce352dab20a68715505c57672aeb1b988a
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-23 00:19:08 +0300

    added lecture 4: Layouts and Links

---
+++ dojo-helpdesk/src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+          <h1>Dojo Helpdesk</h1>
+          <Link href="/">Dashboard</Link>
+          <Link href="/tickets">Tickets</Link>
+        </nav>
+    </div>
+  )
+}
